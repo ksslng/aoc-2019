e9eabdf4b0f8a9436e06aaec15bab51d2fb31f01
Sebastian Kießling <skiessli@student.42.fr>
2019-12-03 11:06:13 +0100

    day02

--- day02_02.py
+++ day02_02.py
@@ -2,14 +2,19 @@ f = open("day02", "r").readline()
 
 t = f.split(",")
 
-code = list(map(int, t))
-code[1] = 12
-code[2] = 2
-pos = 0
-while code[pos] != 99:
-    a = code[code[pos + 1]]
-    b = code[code[pos + 2]]
-    code[code[pos + 3]] = a + b if code[pos] == 1 else a * b
-    pos += 4
+code2 = list(map(int, t))
 
-print(code)
+for i in range(0,99):
+    for j in range(0,99):
+        code = list(code2)
+        code[1] = i
+        code[2] = j
+        pos = 0
+        while code[pos] != 99:
+            a = code[code[pos + 1]]
+            b = code[code[pos + 2]]
+            code[code[pos + 3]] = a + b if code[pos] == 1 else a * b
+            pos += 4
+
+        if code[0] == 19690720:
+            print(i, j)
